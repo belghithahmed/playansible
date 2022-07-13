f30670b15c0a0093cc3773cb8a4e967c032871c0
Ahmed BELGHITH <ahmed.belghith@proxym-it.com>
2022-07-13 14:53:11 +0100

    postgres

--- postgres.sh
+++ postgres.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+PASSWDDB=$1
 set -e
 #create user
-sudo -u postgres createuser $1 with encrypted password $1
+sudo -u postgres psql -c "createuser ${PASSWDDB} with encrypted password '${PASSWDDB}' ;"
 #create db
-sudo -u postgres createdb $1
+sudo -u postgres psql -c "createdb ${PASSWDDB} ;" 
 #grant privileges on db
-grant all privileges on database $1 to $1 ;
+sudo -u postgres psql -c "grant all privileges on database ${PASSWDDB} to ${PASSWDDB} ;"
 
 set +e
