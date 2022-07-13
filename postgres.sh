0d0cf9f91010639b18bd1f9a8ba2b2a91d7b435c
Ahmed BELGHITH <ahmed.belghith@proxym-it.com>
2022-07-13 14:55:17 +0100

    postgres fix

--- postgres.sh
+++ postgres.sh
@@ -2,10 +2,10 @@
 PASSWDDB=$1
 set -e
 #create user
-sudo -u postgres psql -c "createuser ${PASSWDDB} with encrypted password '${PASSWDDB}' ;"
+sudo -u postgres psql -c "createuser ${PASSWDDB} with encrypted password '${PASSWDDB}';"
 #create db
-sudo -u postgres psql -c "createdb ${PASSWDDB} ;" 
+sudo -u postgres psql -c "createdb ${PASSWDDB};" 
 #grant privileges on db
-sudo -u postgres psql -c "grant all privileges on database ${PASSWDDB} to ${PASSWDDB} ;"
+sudo -u postgres psql -c "grant all privileges on database ${PASSWDDB} to ${PASSWDDB};"
 
 set +e
