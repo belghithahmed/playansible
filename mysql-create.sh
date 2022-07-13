e58fa4cff784def7c87a1f1f82939da51681b6c2
Ahmed BELGHITH <ahmed.belghith@proxym-it.com>
2022-07-13 14:06:20 +0100

    shell lf line feed

--- mysql-create.sh
+++ mysql-create.sh
@@ -2,7 +2,9 @@
 
 #mysqladmin -u root -p$1 create $2
 PASSWDDB=$2
+set -e
 mysql -uroot -p$1 -e "CREATE DATABASE ${PASSWDDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 mysql -uroot -p$1 -e "CREATE USER ${PASSWDDB}@'%' IDENTIFIED BY '${PASSWDDB}';"
 mysql -uroot -p$1 -e "GRANT ALL PRIVILEGES ON ${PASSWDDB}.* TO '${PASSWDDB}'@'%';"
 mysql -uroot -p$1 -e "FLUSH PRIVILEGES;"
+set +e
