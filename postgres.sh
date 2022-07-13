9bf6fbfe3ca1878dc91a04dcabde5dacd6c8cec3
Ahmed BELGHITH <ahmed.belghith@proxym-it.com>
2022-07-13 15:07:06 +0100

    postgres fixx

--- postgres.sh
+++ postgres.sh
@@ -4,7 +4,7 @@ set -e
 #create user
 sudo -u postgres psql -c "CREATE USER $PASSWDDB WITH PASSWORD '$PASSWDDB';"
 #create db
-sudo -u postgres psql -c "createdb  $PASSWDDB;" 
+sudo -u postgres psql -c "create database  $PASSWDDB;" 
 #grant privileges on db
 sudo -u postgres psql -c "grant all privileges on database $PASSWDDB to $PASSWDDB;"
 
