809656a967e825a11fb135066b9f4082f679c385
Ahmed BELGHITH <ahmed.belghith@proxym-it.com>
2022-07-13 12:26:49 +0100

    vv

---
+++ mysql-create.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#mysqladmin -u root -p$1 create $2
+PASSWDDB=$2
+
+mysql -uroot -p$1  -e "CREATE DATABASE ${PASSWDDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+mysql -uroot -p$1 -e "CREATE USER ${PASSWDDB}@'%' IDENTIFIED BY '${PASSWDDB}';"
+mysql -uroot -p$1 -e "GRANT ALL PRIVILEGES ON ${PASSWDDB}.* TO '${PASSWDDB}'@'%';"
+mysql -uroot -p$1 -e "FLUSH PRIVILEGES;"
